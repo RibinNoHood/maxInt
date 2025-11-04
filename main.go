f512fa8a6c2ae651793fb1e799b880b173e908fd
rnhqw <rnhqwerty@gmail.com>
2025-11-04 22:42:11 +0300

    s

--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(5)
 
-	for i := 0; i < 5; i++ {
-		go func(a int) {
-			fmt.Println(a)
-			wg.Done()
-		}(i)
-	}
-
-	wg.Wait()
 }
 
 func MaxInt(a, b int) int {
